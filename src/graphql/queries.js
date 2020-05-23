754fe89edc0750e96546d95c4715a6131d945ec1
Lennox <hkao10@asu.edu>
2020-05-22 17:26:44 -0700

    Create a new Project Maple

---
+++ src/graphql/queries.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      code
+      name
+      model
+      spec
+      description
+      url
+      msrp
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        code
+        name
+        model
+        spec
+        description
+        url
+        msrp
+        price
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      code
+      name
+      model
+      spec
+      description
+      url
+      msrp
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      code
+      name
+      model
+      spec
+      description
+      url
+      msrp
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      code
+      name
+      model
+      spec
+      description
+      url
+      msrp
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
